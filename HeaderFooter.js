fc6570848e6d45a7bd201236df3823d092d4656d
Siddhantmi <siddhantmishrabusy949@gmail.com>
2023-01-04 17:14:58 +0530

    made the header responsive

--- HeaderFooter.js
+++ HeaderFooter.js
@@ -25,7 +25,8 @@ class MyHeader extends HTMLElement {
 
         <nav class="navbar navbar-expand-lg navbar-dark py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
             <a href="index.html" class="navbar-brand ms-4 ms-lg-0">
-                <h1 class="fw-bold text-primary m-0">Innovation and Incubation<span class="text-white"> Cell</span></h1>
+                <h1 id ="nav-responsive-big" class="fw-bold text-primary m-0">Innovation and Incubation<span class="text-white"> Cell</span></h1>
+                <h1 id ="nav-responsive-small" class="fw-bold text-primary m-0" >IIC<span class="text-white"> BIET</span></h1>
             </a>
             <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse"
                 data-bs-target="#navbarCollapse">
@@ -44,7 +45,7 @@ class MyHeader extends HTMLElement {
                       </div>
                     </div>
                     <div class="nav-item dropdown">
-                       <a href="../events/index.html" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Innovanza'22</a>
+                       <a href="../events\index.html" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Innovanza'23</a>
                       <div class="dropdown-menu m-0">
                         <a href="../events/CodeEThon.html" class="dropdown-item">Code-E-Thon</a>
                         <a href=" " class="dropdown-item">HackThon</a>
